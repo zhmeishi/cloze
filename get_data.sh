4c70f47258b95da6e8525e70c99ebd0f98187d65
zhmeishi <zhmeishi@gmail.com>
2018-06-09 02:23:20 +0800

    get_data add tmp folder

--- get_data.sh
+++ get_data.sh
@@ -6,5 +6,9 @@ mkdir data
 wget https://polybox.ethz.ch/index.php/s/l2wM4RIyI3pD7Tl/download -O data/train.csv
 wget https://polybox.ethz.ch/index.php/s/02IVLdBAgVcsJAx/download -O data/val.csv
 
+wget https://polybox.ethz.ch/index.php/s/ICsY5IWH3ldtNJb -O tmp.zip
+unzip tmp.zip
+rm tmp.zip
+
 mkdir vec
 wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.en.vec -qO - | head -2000001 > vec/fasttext.vec
